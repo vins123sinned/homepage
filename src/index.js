c197dfff898a9147d88d39e0c230ec5db54d5ee3
Vinson <vinsonhe2@gmail.com>
2025-05-21 09:54:03 -0700

    Add HTML for projects section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,112 @@
+import weatherAppScreenshot from './assets/pictures/screenshots/weather-app.png';
+import libraryScreenshot from './assets/pictures/screenshots/library.png';
+import restaurantPageScreenshot from './assets/pictures/screenshots/restaurant-page.png';
+import todoListScreenshot from './assets/pictures/screenshots/todo-list.png';
+import battleshipScreenshot from './assets/pictures/screenshots/battleship.png';
+import odinRecipesScreenshot from './assets/pictures/screenshots/odin-recipes.png';
 import './styles.css';
+
+const projects = [
+    {
+        img: weatherAppScreenshot,
+        name: 'Weather App',
+        description: 'A weather app utilizing the power of Visual Crossing\'s API and promises to deliver real time data about any city in the world.',
+        githubLink: 'https://github.com/vins123sinned/weather-app',
+        projectLink: 'https://vins123sinned.github.io/weather-app/',
+    },
+    {
+        img: libraryScreenshot,
+        name: 'Library',
+        description: 'A simple project utilizing objects and object constructors to create and display a list of books. Books are stored as objects in an array which will then be used to display the data to the user.',
+        githubLink: 'https://github.com/vins123sinned/Library',
+        projectLink: 'https://vins123sinned.github.io/Library/',
+    },
+    {
+        img: restaurantPageScreenshot,
+        name: 'Restaurant Page',
+        description: 'A website rendered using only DOM manipulation. Each website section will be created using DOM elements and styled using CSS',
+        githubLink: 'https://github.com/vins123sinned/restaurant-page',
+        projectLink: 'https://vins123sinned.github.io/restaurant-page/',
+    },
+    {
+        img: todoListScreenshot,
+        name: 'Todo List',
+        description: 'A simple todo list that allows for creating, reading, updating, and deleting each todos.',
+        githubLink: 'https://github.com/vins123sinned/todo-list',
+        projectLink: 'https://vins123sinned.github.io/todo-list/',
+    },
+    {
+        img: battleshipScreenshot,
+        name: 'Battleship',
+        description: 'Battleship game developed using Test Driven Development (TDD)',
+        githubLink: 'https://github.com/vins123sinned/battleship',
+        projectLink: 'https://vins123sinned.github.io/battleship/',
+    },
+    {
+        img: odinRecipesScreenshot,
+        name: 'Odin Recipes',
+        description: 'My very first website after a very, very long hiatus! Enjoy my special flan recipe courtesy of allRecipes!',
+        githubLink: 'https://github.com/vins123sinned/odin-recipes',
+        projectLink: 'https://vins123sinned.github.io/odin-recipes/',
+    }
+];
+
+function createProjectCell(project) {
+    const main = document.querySelector('main');
+    const projectCell = document.createElement('div');
+    // screenshot could use resolution switching!
+    const screenshot = document.createElement('img');
+    const projectInformation = document.createElement('div');
+    const projectHeader = document.createElement('header');
+
+    const projectName = document.createElement('h3');
+    const githubLink = document.createElement('a');
+    const projectLink = document.createElement('a');
+    const githubIcon = document.createElement('img');
+    const shareIcon = document.createElement('span');
+    const projectDescription = document.createElement('p');
+    
+    projectCell.classList.add('project-cell');
+    screenshot.classList.add('project-screenshot');
+    projectInformation.classList.add('project-information-container');
+    projectHeader.classList.add('project-header');
+    projectName.classList.add('project-name');
+    githubIcon.classList.add('project-icon');
+    shareIcon.classList.add('project-icon', 'material-symbols-outlined');
+    projectDescription.classList.add('project-description');
+
+    screenshot.src = project.img;
+    screenshot.alt = '';
+    projectName.textContent = project.name;
+    projectDescription.textContent = project.description;
+    githubLink.href = project.githubLink;
+    githubLink.target = '_blank';
+    githubLink.rel = 'noopener noreferrer';
+    projectLink.href = project.projectLink;
+    projectLink.target = '_blank';
+    projectLink.rel = 'noopener noreferrer';
+    githubIcon.src = 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg';
+    githubIcon.alt = 'github';
+    shareIcon.textContent = 'open_in_new';
+
+    githubLink.appendChild(githubIcon);
+    projectLink.appendChild(shareIcon);
+
+    projectHeader.appendChild(projectName);
+    projectHeader.appendChild(githubLink);
+    projectHeader.appendChild(projectLink);
+
+    projectInformation.appendChild(projectHeader);
+    projectInformation.appendChild(projectDescription);
+
+    projectCell.appendChild(screenshot);
+    projectCell.appendChild(projectInformation);
+
+    main.appendChild(projectCell);
+}
+
+(function populateProjects() {
+    projects.forEach((project) => {
+        createProjectCell(project);
+    });
+})();
